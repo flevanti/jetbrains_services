b0f2ce9560f2741961158972e7fd69318412a256
flevanti <frenke@frenke.it>
2018-06-17 21:54:48 +0100

    implemented teamcity... more pain... and websockets

--- manager.sh
+++ manager.sh
@@ -7,6 +7,7 @@ fi
 export HUB_DOMAIN=jbhub.deploy.me.uk
 export YOUTRACK_DOMAIN=jbyoutrack.deploy.me.uk
 export UPSOURCE_DOMAIN=jbupsource.deploy.me.uk
+export TEAMCITY_DOMAIN=jbteamcity.deploy.me.uk
 export DATA_FOLDER=./shared
 
 if [ "$1" = "down" ] 
@@ -16,8 +17,10 @@ then
 fi
 
 
+if [ "$1" = "up" ] 
+then
 
-if [[ ! -e "$DATA_FOLDER/foldersok" ]]; then
+	if [[ ! -e "$DATA_FOLDER/foldersok" ]]; then
        echo "FOLDERS DO NOT EXIST, CREATING THEM...."
        mkdir -p -m 777 "$DATA_FOLDER/upsource/data" "$DATA_FOLDER/upsource/logs" "$DATA_FOLDER/upsource/conf" "$DATA_FOLDER/upsource/backups"
        $SUDO chown -R 13001:13001 "$DATA_FOLDER/upsource/data" "$DATA_FOLDER/upsource/logs" "$DATA_FOLDER/upsource/conf" "$DATA_FOLDER/upsource/backups"
@@ -28,12 +31,20 @@ if [[ ! -e "$DATA_FOLDER/foldersok" ]]; then
        mkdir -p -m 777 "$DATA_FOLDER/hub/data" "$DATA_FOLDER/hub/logs" "$DATA_FOLDER/hub/conf" "$DATA_FOLDER/hub/backups"
        $SUDO chown -R 13001:13001 "$DATA_FOLDER/hub/data" "$DATA_FOLDER/hub/logs" "$DATA_FOLDER/hub/conf" "$DATA_FOLDER/hub/backups"
 
+       mkdir -p -m 777 "$DATA_FOLDER/teamcity/data" "$DATA_FOLDER/teamcity/logs" "$DATA_FOLDER/teamcity/conf" "$DATA_FOLDER/teamcity/backups"
+       #$SUDO chown -R 13001:13001 "$DATA_FOLDER/teamcity/data" "$DATA_FOLDER/teamcity/logs" "$DATA_FOLDER/teamcity/conf" "$DATA_FOLDER/teamcity/backups"
+
        $sudo touch "$DATA_FOLDER/foldersok"
+	fi
+
+	if [[ ! -e "$DATA_FOLDER/foldersok" ]]; then
+		echo "SOMETHING IS WRONG, LOOKS LIKE I WASN'T ABLE TO PROPERLY CREATE FOLDERS..."
+		exit;
+	fi
+
+	$sudo docker-compose up --build
+
+    exit
 fi
 
-if [[ ! -e "$DATA_FOLDER/foldersok" ]]; then
-	echo "SOMETHING IS WRONG, LOOKS LIKE I WASN'T ABLE TO PROPERLY CREATE FOLDERS..."
-	exit;
-fi
-
-$sudo docker-compose up --build
+echo "up or down???"
